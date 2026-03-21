1c6076e630d4507d4d4c976215c4198a41891dea
Prashant Dev Singh <prashantdev4u@gmail.com>
2026-03-21 18:28:48 +0530

    Initial commit - Flask 2-tier Docker app

---
+++ app/app.py
@@ -0,0 +1,71 @@
+import time
+from flask import Flask, render_template, request, redirect
+from models import db, User
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+
+# ✅ WAIT FOR DB
+def wait_for_db():
+    for i in range(10):
+        try:
+            with app.app_context():
+                db.create_all()
+            print("✅ Database connected!")
+            return
+        except Exception as e:
+            print("⏳ Waiting for DB...")
+            time.sleep(3)
+
+wait_for_db()
+
+# ✅ NEW WAY (Flask 3 compatible)
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():   
+   return render_template('index.html')
+ #return "Flask is working 🚀"
+
+@app.route('/users')
+def users():
+    all_users = User.query.all()
+    return render_template('users.html', users=all_users)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        user = User(name=name, email=email)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/users')
+    return render_template('add_user.html')
+
+@app.route('/delete/<int:id>')
+def delete_user(id):
+    user = User.query.get(id)
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+    return redirect('/users')
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit_user(id):
+    user = User.query.get_or_404(id)
+
+    if request.method == 'POST':
+        user.name = request.form['name']
+        user.email = request.form['email']
+        db.session.commit()
+        return redirect('/users')
+    return render_template('edit_user.html', user=user)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
---
+++ app/config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL",
+        "postgresql://postgres:postgres@db:5432/mydb"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
